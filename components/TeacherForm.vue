6ab949a8d200321618262e92e5ae1c71bc2b4b4f
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-08-11 22:08:08 -0400

    teacher form finish

--- components/TeacherForm.vue
+++ components/TeacherForm.vue
@@ -4,13 +4,39 @@
     <div><label>Nombre:</label> <input type="text" v-model="teacher.teacherName"></div>
     <div><label>Apellidos:</label> <input type="text" v-model="teacher.surname"></div>
     <div><label>RUT:</label> <input type="text" v-model="teacher.rut"></div>
-    <div><label>Asignaturas:</label> <input type="text" v-model="subject"><button>Agregar</button></div>
+    <div><label>Asignaturas:</label> <input type="text" v-model="subject"><button @click="handleSubject">Agregar</button></div>
+    <div>
+      <ul>
+        <li v-for="(elm, index) in teacher.asignature" :key="index">{{ elm }}</li>
+      </ul>
+    </div>
     <input type="checkbox" v-model="teacher.doc">Documentacion entregada
-    <button>Agregar</button>
+    <button @click="handleAddTeacher">Agregar</button>
   </section>
 
   <section>
     <h3>Listado de Profesores</h3>
+    <table>
+      <tr>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>RUT</th>
+        <th>Asignaturas</th>
+        <th>Documentacion Entregada</th>
+      </tr>
+      <tr v-for="elm in teachers" :key="elm.rut">
+        <th>{{ elm.teacherName }}</th>
+        <th>{{ elm.surname }}</th>
+        <th>{{ elm.rut }}</th>
+        <th>
+          <ul>
+            <li v-for="(item, index) in elm.asignature" :key="index">{{ item }}</li>
+          </ul>
+        </th>
+        <th v-if="elm.doc">Entregada</th>
+        <th v-else>No Entregada</th>
+      </tr>
+    </table>
   </section>
 </template>
  
@@ -44,6 +70,28 @@
     let teachers:Ref<Array<ITeacher>> = ref ([])
 
     let subject:Ref<string> = ref ('')
+
+    const handleSubject = () => {
+      teacher.value.asignature.push(subject.value)
+      subject.value = '' // Para limpiar el input
+    }
+
+    const handleAddTeacher = () => {
+      teachers.value.push({
+        teacherName: teacher.value.teacherName,
+        surname: teacher.value.surname,
+        rut: teacher.value.rut,
+        asignature: teacher.value.asignature,
+        doc: teacher.value.doc
+      })
+      teacher.value = {
+        teacherName: '',
+        surname: '',
+        rut: '',
+        asignature: [],
+        doc: false
+      }
+    }
 </script>
 
 <style scoped>
